c3803e3d4cf1c65d4bce633a2ee8f20dfdf1df2a
Shagun Madhikarmi <shagun@ustwo.com>
2019-11-25 17:47:33 +0000

    Now displaying image and text of album in detail modal

--- MacCatalystDemo/MacCatalystDemo/AlbumDetailViewController.swift
+++ MacCatalystDemo/MacCatalystDemo/AlbumDetailViewController.swift
@@ -7,7 +7,50 @@
 //
 
 import UIKit
+import ImageManager
 
+ 
+class AlbumView: UIView {
+    
+    let imageView = UIImageView()
+    let label = UILabel()
+
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        addSubview(imageView)
+        addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3),
+            imageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.3),
+        ])
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+
+    }
+}
 class AlbumViewController: UIViewController {
     
     private let album: Album
@@ -23,21 +66,25 @@ class AlbumViewController: UIViewController {
     }
     
     override func loadView() {
-        self.view = UIView()
+        self.view = AlbumView()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = (index%2 == 0) ? .red : .purple
+        let albumView = (view as! AlbumView)
+        albumView.label.text = album.albumName
+        navigationItem.title = album.albumName
         
-        let label = UILabel(frame: .zero)
-        view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
-        label.text = album.albumName
+        guard let imageURL = album.albumImageURL else { return }
+        ImageManager().loadImage(for: imageURL, completion: { (result) in
+            switch result {
+            case .success(let image):
+                albumView.imageView.image = image
+            case .failure(_):
+                albumView.imageView.image = nil
+            }
+        })
     }
 }
 
