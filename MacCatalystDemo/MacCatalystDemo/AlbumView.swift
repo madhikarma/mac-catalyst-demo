8272c1dc7b1582f266bce6ecaa7f0867f6835ceb
Shagun Madhikarmi <shagun@ustwo.com>
2019-11-25 17:54:23 +0000

    Tidy up files

---
+++ MacCatalystDemo/MacCatalystDemo/AlbumView.swift
@@ -0,0 +1,51 @@
+//
+//  AlbumView.swift
+//  MacCatalystDemo
+//
+//  Created by Beta on 25/11/2019.
+//  Copyright © 2019 Beta. All rights reserved.
+//
+
+import UIKit
+ 
+class AlbumView: UIView {
+    
+    let imageView = UIImageView()
+    let label = UILabel()
+
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        addSubview(imageView)
+        addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3),
+            imageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.3),
+        ])
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+
+    }
+}
---
+++ MacCatalystDemo/MacCatalystDemo/AlbumViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AlbumViewController.swift
+//  MacCatalystDemo
+//
+//  Created by Beta on 25/11/2019.
+//  Copyright © 2019 Beta. All rights reserved.
+//
+
+import UIKit
+import ImageManager
+
+class AlbumViewController: UIViewController {
+    
+    private let album: Album
+    var index: Int = 0
+    private let imageManager: ImageManager
+    
+    init(album: Album, imageManager: ImageManager) {
+        self.album = album
+        self.imageManager = imageManager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = AlbumView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = (index%2 == 0) ? .red : .purple
+        let albumView = (view as! AlbumView)
+        albumView.label.text = album.albumName
+        navigationItem.title = album.albumName
+        
+        guard let imageURL = album.albumImageURL else { return }
+        imageManager.loadImage(for: imageURL, completion: { (result) in
+            switch result {
+            case .success(let image):
+                albumView.imageView.image = image
+            case .failure(_):
+                albumView.imageView.image = nil
+            }
+        })
+    }
+}
+
+
+
